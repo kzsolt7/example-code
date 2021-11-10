2ed86e3bc71a9dbc1f802ed84d13f62c47ee18fa
atjager <66367202+atjager@users.noreply.github.com>
2021-11-10 14:27:12 +0100

    Update UserTable.vue

--- ui/src/components/user/UserTable.vue
+++ ui/src/components/user/UserTable.vue
@@ -1,8 +1,8 @@
 <template>
   <v-data-table
       :headers="headers"
-      :items="desserts"
-      sort-by="calories"
+      :items="users"
+      sort-by="permissionGroup"
       class="elevation-1"
   >
     <template v-slot:top>
@@ -28,7 +28,7 @@
                 v-bind="attrs"
                 v-on="on"
             >
-              New Item
+              New user
             </v-btn>
           </template>
           <v-card>
@@ -46,7 +46,7 @@
                   >
                     <v-text-field
                         v-model="editedItem.name"
-                        label="Dessert name"
+                        label="Username"
                     ></v-text-field>
                   </v-col>
                   <v-col
@@ -55,8 +55,8 @@
                       md="4"
                   >
                     <v-text-field
-                        v-model="editedItem.calories"
-                        label="Calories"
+                        v-model="editedItem.permissionGroup"
+                        label="Permission group"
                     ></v-text-field>
                   </v-col>
                   <v-col
@@ -65,8 +65,8 @@
                       md="4"
                   >
                     <v-text-field
-                        v-model="editedItem.fat"
-                        label="Fat (g)"
+                        v-model="editedItem.state"
+                        label="State"
                     ></v-text-field>
                   </v-col>
                   <v-col
@@ -75,8 +75,8 @@
                       md="4"
                   >
                     <v-text-field
-                        v-model="editedItem.carbs"
-                        label="Carbs (g)"
+                        v-model="editedItem.created"
+                        label="Created"
                     ></v-text-field>
                   </v-col>
                   <v-col
@@ -84,10 +84,7 @@
                       sm="6"
                       md="4"
                   >
-                    <v-text-field
-                        v-model="editedItem.protein"
-                        label="Protein (g)"
-                    ></v-text-field>
+
                   </v-col>
                 </v-row>
               </v-container>
@@ -159,32 +156,31 @@ export default {
     dialogDelete: false,
     headers: [
       {
-        text: 'Dessert (100g serving)',
+        text: 'Username',
         align: 'start',
         sortable: false,
         value: 'name',
       },
-      { text: 'Calories', value: 'calories' },
-      { text: 'Fat (g)', value: 'fat' },
-      { text: 'Carbs (g)', value: 'carbs' },
-      { text: 'Protein (g)', value: 'protein' },
+      { text: 'Permission group', value: 'permissionGroup' },
+      { text: 'State', value: 'state' },
+      { text: 'Created', value: 'created' },
       { text: 'Actions', value: 'actions', sortable: false },
     ],
-    desserts: [],
+    users: [],
     editedIndex: -1,
     editedItem: {
       name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+      permissionGroup: '',
+      state: '',
+      created: '',
+      protein: '',
     },
     defaultItem: {
       name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+      permissionGroup: '',
+      state: '',
+      created: '',
+      protein: '',
     },
   }),
 
@@ -209,94 +205,39 @@ export default {
 
   methods: {
     initialize () {
-      this.desserts = [
+      this.users = [
         {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
+          name: 'Lacika',
+          permissionGroup: 'Technológus',
+          state: 'Online',
+          created: '2021-11-11',
+
         },
         {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
+          name: 'Béluka',
+          permissionGroup: 'Admin',
+          state: 'Offline',
+          created: '2021-10-29',
+
         },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-        },
+
       ]
     },
 
     editItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
+      this.editedIndex = this.users.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialog = true
     },
 
     deleteItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
+      this.editedIndex = this.users.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialogDelete = true
     },
 
     deleteItemConfirm () {
-      this.desserts.splice(this.editedIndex, 1)
+      this.users.splice(this.editedIndex, 1)
       this.closeDelete()
     },
 
@@ -318,9 +259,9 @@ export default {
 
     save () {
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        Object.assign(this.users[this.editedIndex], this.editedItem)
       } else {
-        this.desserts.push(this.editedItem)
+        this.users.push(this.editedItem)
       }
       this.close()
     },
