471ce44876c9625a5ab2ad18435d95b5dd8efc9c
Kürti Zsolt <zsolt.kurt@gmail.com>
2021-11-10 14:06:41 +0100

    user list

---
+++ ui/src/views/user/UserList.vue
@@ -0,0 +1,18 @@
+<template>
+  <user-table></user-table>
+</template>
+
+<script>
+import UserTable from "@/components/user/UserTable";
+
+export default {
+  name: "UserList",
+  components:{
+    UserTable
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
