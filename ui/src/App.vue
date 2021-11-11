5089f433209ec21aa0442d188e54d68ac975a6b7
Kürti Zsolt <zsolt.kurt@gmail.com>
2021-11-11 15:18:00 +0100

    appbar user icon

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -14,17 +14,20 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-account-circle-outline</v-icon>
+          </v-btn>
+        </template>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+        <v-list>
+          <v-list-item @click="() => {}">
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <sidebar :drawer="drawer" @close-drawer="drawer = false"></sidebar>
