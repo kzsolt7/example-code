443a1b491de19517f83b2a62dbfd718d6f7f55f0
atjager <66367202+atjager@users.noreply.github.com>
2021-11-12 09:08:11 +0100

    Dialog removed

--- ui/src/components/user/GroupTable.vue
+++ ui/src/components/user/GroupTable.vue
@@ -16,89 +16,6 @@
             vertical
         ></v-divider>
         <v-spacer></v-spacer>
-        <v-dialog
-            v-model="dialog"
-            max-width="500px"
-        >
-          <template v-slot:activator="{}">
-            <v-btn
-                color="teal"
-                dark
-                class="mb-2"
-                to="/add-new-group"
-            >
-              New group
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.name"
-                        label="Group name"
-                    ></v-text-field>
-                  </v-col>
-
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.state"
-                        label="State"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.created"
-                        label="Created"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
@@ -142,7 +59,6 @@
 export default {
   name: "GroupTable",
   data: () => ({
-    dialog: false,
     dialogDelete: false,
     headers: [
       {
@@ -156,13 +72,7 @@ export default {
       { text: 'Actions', value: 'actions', sortable: false },
     ],
     users: [],
-    editedIndex: -1,
-    editedItem: {
-      name: '',
-      state: '',
-      created: '',
-      protein: '',
-    },
+
     defaultItem: {
       name: '',
       state: '',
@@ -178,9 +88,6 @@ export default {
   },
 
   watch: {
-    dialog (val) {
-      val || this.close()
-    },
     dialogDelete (val) {
       val || this.closeDelete()
     },
@@ -215,13 +122,6 @@ export default {
       ]
     },
 
-    editItem (item) {
-      this.editedIndex = this.users.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      //this.dialog = true
-      this.$router.push("/edit-group")
-    },
-
     deleteItem (item) {
       this.editedIndex = this.users.indexOf(item)
       this.editedItem = Object.assign({}, item)
@@ -233,13 +133,6 @@ export default {
       this.closeDelete()
     },
 
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
 
     closeDelete () {
       this.dialogDelete = false
@@ -247,15 +140,6 @@ export default {
         this.editedItem = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
       })
-    },
-
-    save () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.users[this.editedIndex], this.editedItem)
-      } else {
-        this.users.push(this.editedItem)
-      }
-      this.close()
     },
   },
 }
--- ui/src/components/user/UserTable.vue
+++ ui/src/components/user/UserTable.vue
@@ -16,98 +16,6 @@
             vertical
         ></v-divider>
         <v-spacer></v-spacer>
-        <v-dialog
-            v-model="dialog"
-            max-width="500px"
-        >
-          <template v-slot:activator="{ }">
-            <v-btn
-                color="teal"
-                dark
-                class="mb-2"
-                to="/add-new-user"
-            >
-              New user
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.name"
-                        label="Username"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.permissionGroup"
-                        label="Permission group"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.state"
-                        label="State"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-                    <v-text-field
-                        v-model="editedItem.created"
-                        label="Created"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                  >
-
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
@@ -151,7 +59,6 @@
 export default {
   name: "UserTable",
   data: () => ({
-    dialog: false,
     dialogDelete: false,
     headers: [
       {
@@ -166,14 +73,6 @@ export default {
       { text: 'Actions', value: 'actions', sortable: false },
     ],
     users: [],
-    editedIndex: -1,
-    editedItem: {
-      name: '',
-      permissionGroup: '',
-      state: '',
-      created: '',
-      protein: '',
-    },
     defaultItem: {
       name: '',
       permissionGroup: '',
@@ -190,9 +89,6 @@ export default {
   },
 
   watch: {
-    dialog (val) {
-      val || this.close()
-    },
     dialogDelete (val) {
       val || this.closeDelete()
     },
@@ -228,7 +124,6 @@ export default {
       this.editedIndex = this.users.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.$router.push("/edit-user")
-      //this.dialog = true
     },
 
     deleteItem (item) {
@@ -242,13 +137,6 @@ export default {
       this.closeDelete()
     },
 
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
 
     closeDelete () {
       this.dialogDelete = false
@@ -256,15 +144,6 @@ export default {
         this.editedItem = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
       })
-    },
-
-    save () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.users[this.editedIndex], this.editedItem)
-      } else {
-        this.users.push(this.editedItem)
-      }
-      this.close()
     },
   },
 }
