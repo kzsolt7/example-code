c932c1fa1df4611829299f312d45789d7b4fd8c2
atjager <66367202+atjager@users.noreply.github.com>
2021-11-12 09:50:13 +0100

    Example api endpoint added

--- backend/src/main/java/com/cavityeye/backend/user/controller/UserController.java
+++ backend/src/main/java/com/cavityeye/backend/user/controller/UserController.java
@@ -1,19 +1,28 @@
 package com.cavityeye.backend.user.controller;
 
 import com.cavityeye.backend.user.dto.UserDto;
+import com.cavityeye.backend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping()
-    public UserDto getUsers() {
-        return null;
-    }
+    private final UserService userService;
+
+
 
     @PostMapping()
     public void createUser(@RequestBody UserDto user) {
-        System.out.println(user);
+        userService.saveUser(user);
+    }
+
+    @GetMapping()
+    public UserDto getUserById(@RequestParam String id){
+        return userService.findUserById(id);
     }
 }
--- backend/src/main/java/com/cavityeye/backend/user/service/UserService.java
+++ backend/src/main/java/com/cavityeye/backend/user/service/UserService.java
@@ -1,7 +1,26 @@
 package com.cavityeye.backend.user.service;
 
+import com.cavityeye.backend.user.dto.UserDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    public void saveUser(UserDto user){
+        System.out.println(user);
+    }
+
+    public UserDto findUserById(String id){
+        System.out.println(id);
+        var user = new UserDto();
+        user.setUserName("Jani");
+        user.setEmail("Valami@gmail.com");
+        String[] permissions = {"editPfill","createEditRef"};
+        String[] permissionGroups = {"enginner","operator"};
+        user.setPermissions(permissions);
+        user.setPassword("janika345");
+        user.setPermissionGroups(permissionGroups);
+        user.setState("Active");
+        return user;
+    }
 }
