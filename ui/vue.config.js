8d3ef5d7d8e4bd07a9055d30f5189f0173fb751a
atjager <66367202+atjager@users.noreply.github.com>
2021-12-06 16:58:54 +0100

    7070

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "devServer": {
     "proxy": {
       "/api": {
-        "target": "http://localhost:7071",
+        "target": "http://localhost:7070",
         "ws": true,
         "changeOrigin": true
       }
