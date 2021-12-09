70cd6979a846cbb33e069c1f9cbf0af6927ea3a6
Kürti Zsolt <zsolt.kurt@gmail.com>
2021-12-09 15:28:49 +0100

    redirect

--- backend/src/main/java/com/cavityeye/backend/HomeController.java
+++ backend/src/main/java/com/cavityeye/backend/HomeController.java
@@ -6,8 +6,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/about")
+    @RequestMapping(value = {
+            "/about",
+            "/device-list",
+            "/user-list",
+            "/add-new-user",
+            "/edit-user/**",
+            "/notification",
+            "/add-new-notification-group",
+            "/edit-notification-group/**",
+            "/permission",
+            "/add-new-permission-group",
+            "/edit-permission-group/**",
+            "/export-import",
+            "/realtime-monitoring",
+            "/clamp-analysis",
+            "/network-management",
+            "/system-update",
+    })
     public String aboutForward() {
         return "forward:/";
     }
+
 }
